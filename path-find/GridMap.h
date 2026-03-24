31cc5f6ba28a4f70f05c7ae75546444d25ce5b33
Umut <umut.clsc01@outlook.com>
2026-03-24 19:45:58 +0300

    gridmap added

---
+++ path-find/GridMap.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <string>
+class GridMap
+{
+public:
+	float resolution;
+	int width;
+	int height;
+	int mapMaxVal;
+	int total_size;
+	std::string magic_word;
+	std::string map_path;
+	std::vector<bool> map_data;
+	GridMap(std::string& map_path, float res);
+	void ReadFile();
+
+};
+
