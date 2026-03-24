18ab6dc4fbee731f50f47dac6a594c164c7e96c7
Umut <umut.clsc01@outlook.com>
2026-03-24 19:46:07 +0300

    gridmap added

---
+++ path-find/GridMap.cpp
@@ -0,0 +1,26 @@
+#include "GridMap.h"
+#include <fstream>
+#include <iostream>
+GridMap::GridMap(std::string& map_path, float res)
+{
+	this->map_path = map_path;
+	this->resolution = res;
+	std::fstream file(map_path);
+	if (!file.is_open()) throw std::runtime_error("The file " + map_path + " could not be opened");
+	else std::cout << "File opened successfully" << std::endl;
+}
+
+void GridMap::ReadFile()
+{
+	std::fstream file(map_path);
+	if (!file.is_open()) throw std::runtime_error("The file " + map_path + " could not be opened");
+	file >> magic_word >> width >> height >> mapMaxVal;
+	total_size = width * height;
+	for (int i = 0; i < total_size; i++) {
+		int curr_val;
+		file >> curr_val;
+		map_data[i] = (curr_val > 0); // bigger than 0 means white 
+	}
+
+
+}
