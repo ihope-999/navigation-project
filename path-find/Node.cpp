f48dced370f8a35a9d82063aeb273eb6358b0db9
Umut <umut.clsc01@outlook.com>
2026-03-27 18:37:43 +0300

    Added member variables and functions

--- path-find/Node.cpp
+++ path-find/Node.cpp
@@ -1,3 +1,16 @@
+#include <memory>
+#define M_PI 3.14
 struct Node {
-
+	float x; float y; float degree;
+	float g; float h;
+	int ix; int iy; int idegree;
+	std::shared_ptr<Node> parent = nullptr;
+	float get_total_cost() const {
+		return g + h;
+	}
+	void discretize(float res, float dtheta) {
+		ix = x / res;
+		iy = y / res;
+		idegree = (degree + M_PI) / dtheta;
+	}
 };
